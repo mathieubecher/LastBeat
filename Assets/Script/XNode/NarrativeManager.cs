ad73a64d63b88337652ef4777056e2ca84afd436
Becher <mbelkhenchir@gmail.com>
2021-07-02 22:01:05 +0200

    [ADD] XNodes graph first iteration

---
+++ Assets/Script/XNode/NarrativeManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using XNode;
+using XNodeEditor;
+
+namespace DialogSystem
+{
+    public class NarrativeManager : MonoBehaviour
+    {
+        [SerializeField] private NarrativeGraph _narrativeGraph;
+        [SerializeField] private Node _activeSegment;
+
+        void Start()
+        {
+            foreach (Node node in _narrativeGraph.nodes)
+            {
+                if (!node.GetInputPort("input").IsConnected)
+                {
+                    _activeSegment = node;
+                    break;
+                }
+            }
+        }
+        
+    }
+}
